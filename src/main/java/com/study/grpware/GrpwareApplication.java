1e3d642792625a0d41545666b9157ed5a43441e9
yonse <yonsei1994@gmail.com>
2023-11-18 17:51:37 +0900

    Initial commit

---
+++ src/main/java/com/study/grpware/GrpwareApplication.java
@@ -0,0 +1,13 @@
+package com.study.grpware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/grpware/GrpwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.grpware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
